72d0df37d0cbf505269c35292c53b555ca731283
iboying <weboying@gmail.com>
2016-07-14 11:57:09 +0800

    complete limit sail

---
+++ app/src/main/java/com/tallty/smart_life_android/adapter/HomeLimitSailAdapter.java
@@ -0,0 +1,72 @@
+package com.tallty.smart_life_android.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.tallty.smart_life_android.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kang on 16/7/14.
+ * 首页-限量销售-适配器
+ */
+public class HomeLimitSailAdapter extends RecyclerView.Adapter<HomeLimitSailAdapter.ProductViewHolder> {
+    private ArrayList<String> names;
+    private ArrayList<String> prices;
+    private ArrayList<Integer> photos;
+    private Context context;
+
+    public HomeLimitSailAdapter(Context context, ArrayList<String> names,
+                                ArrayList<String> prices, ArrayList<Integer> photos) {
+        this.context = context;
+        this.names = names;
+        this.prices = prices;
+        this.photos = photos;
+    }
+
+    @Override
+    public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(context).inflate(R.layout.item_home_limit_sail, parent, false);
+        return new ProductViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ProductViewHolder holder, int position) {
+        Glide.with(context).load(photos.get(position)).into(holder.photo);
+        holder.name.setText(names.get(position));
+        holder.price.setText(prices.get(position));
+        if (position == getItemCount()-1) {
+            holder.line.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return names.size();
+    }
+
+    class ProductViewHolder extends RecyclerView.ViewHolder {
+        private ImageView photo;
+        private TextView name;
+        private TextView price;
+        private Button detail;
+        private View line;
+
+        public ProductViewHolder(View itemView) {
+            super(itemView);
+            name = (TextView) itemView.findViewById(R.id.product_name);
+            price = (TextView) itemView.findViewById(R.id.product_price);
+            detail = (Button) itemView.findViewById(R.id.detail_btn);
+            photo = (ImageView) itemView.findViewById(R.id.product_photo);
+            line = itemView.findViewById(R.id.product_line);
+        }
+    }
+}
--- app/src/main/java/com/tallty/smart_life_android/fragment/home/LimitSail.java
+++ app/src/main/java/com/tallty/smart_life_android/fragment/home/LimitSail.java
@@ -4,6 +4,8 @@ package com.tallty.smart_life_android.fragment.home;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,7 +13,10 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.tallty.smart_life_android.R;
+import com.tallty.smart_life_android.adapter.HomeLimitSailAdapter;
 import com.tallty.smart_life_android.base.BaseBackFragment;
+
+import java.util.ArrayList;
 
 /**
  * 首页-限量销售
@@ -21,6 +26,25 @@ public class LimitSail extends BaseBackFragment {
 
     private Toolbar toolbar;
     private TextView toolbar_title;
+    private RecyclerView recyclerView;
+
+    private ArrayList<String> names = new ArrayList<String>() {
+        {
+            add("《从你的全世界路过》张嘉佳 著");add("《从你的全世界路过》张嘉佳 著");
+            add("《从你的全世界路过》张嘉佳 著");add("《从你的全世界路过》张嘉佳 著");
+        }
+    };
+    private ArrayList<String> prices = new ArrayList<String>() {
+        {
+            add("今日价: 40.00");add("今日价: 40.00");add("今日价: 40.00");add("今日价: 40.00");
+        }
+    };
+    private ArrayList<Integer> photos = new ArrayList<Integer>() {
+        {
+            add(R.drawable.product_demo);add(R.drawable.product_demo);add(R.drawable.product_demo);
+            add(R.drawable.product_demo);
+        }
+    };
 
     public static LimitSail newInstance(String title) {
         Bundle args = new Bundle();
@@ -48,6 +72,7 @@ public class LimitSail extends BaseBackFragment {
     protected void initView() {
         toolbar = getViewById(R.id.toolbar);
         toolbar_title = getViewById(R.id.toolbar_title);
+        recyclerView = getViewById(R.id.limit_sail_list);
     }
 
     @Override
@@ -59,6 +84,11 @@ public class LimitSail extends BaseBackFragment {
     protected void afterAnimationLogic() {
         initBackToolbar(toolbar);
         toolbar_title.setText(mName);
+        // 初始化列表
+        RecyclerView.LayoutManager manager = new LinearLayoutManager(context);
+        recyclerView.setLayoutManager(manager);
+        HomeLimitSailAdapter adapter = new HomeLimitSailAdapter(context, names, prices, photos);
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
