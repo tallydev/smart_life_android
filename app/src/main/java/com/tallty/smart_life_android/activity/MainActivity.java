a7b3f2cf43ecae59582483bb8bd30baea694993d
iboying <weboying@gmail.com>
2016-12-18 19:34:13 +0800

    complete pay logic

--- app/src/main/java/com/tallty/smart_life_android/activity/MainActivity.java
+++ app/src/main/java/com/tallty/smart_life_android/activity/MainActivity.java
@@ -1,9 +1,15 @@
 package com.tallty.smart_life_android.activity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.joanzapata.iconify.Iconify;
 import com.joanzapata.iconify.fonts.FontAwesomeModule;
+import com.pingplusplus.android.Pingpp;
+import com.pingplusplus.android.PingppLog;
+import com.tallty.smart_life_android.App;
 import com.tallty.smart_life_android.R;
 import com.tallty.smart_life_android.fragment.MainFragment;
 
@@ -23,6 +29,7 @@ public class MainActivity extends SupportActivity {
 
         // 加载图标
         Iconify.with(new FontAwesomeModule());
+        PingppLog.DEBUG = true;
     }
 
     @Override
@@ -40,5 +47,29 @@ public class MainActivity extends SupportActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //支付页面返回处理
+        if (requestCode == Pingpp.REQUEST_CODE_PAYMENT) {
+            if (resultCode == Activity.RESULT_OK) {
+                String result = data.getExtras().getString("pay_result");
+
+                // 处理返回值
+                // "success" - 支付成功
+                // "fail"    - 支付失败
+                // "cancel"  - 取消支付
+                // "invalid" - 支付插件未安装（一般是微信客户端未安装的情况）
+
+                String errorMsg = data.getExtras().getString("error_msg"); // 错误信息
+                String extraMsg = data.getExtras().getString("extra_msg"); // 错误信息
+                Log.i(App.TAG, "结果:===》"+result);
+                Log.i(App.TAG, "错误:===》"+errorMsg);
+                Log.i(App.TAG, "额外:===》"+extraMsg);
+                Log.i(App.TAG, data.getExtras().getInt("code") + "==========");
+            }
+        }
     }
 }
--- app/src/main/java/com/tallty/smart_life_android/fragment/cart/PayOrder.java
+++ app/src/main/java/com/tallty/smart_life_android/fragment/cart/PayOrder.java
@@ -1,37 +1,62 @@
 package com.tallty.smart_life_android.fragment.cart;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import com.google.gson.JsonElement;
+import com.pingplusplus.android.Pingpp;
+import com.tallty.smart_life_android.App;
 import com.tallty.smart_life_android.Const;
+import com.tallty.smart_life_android.Engine.Engine;
 import com.tallty.smart_life_android.R;
+import com.tallty.smart_life_android.activity.MainActivity;
 import com.tallty.smart_life_android.base.BaseBackFragment;
 import com.tallty.smart_life_android.event.StartBrotherEvent;
 import com.tallty.smart_life_android.event.SwitchTabFragment;
 import com.tallty.smart_life_android.fragment.MainFragment;
 import com.tallty.smart_life_android.fragment.me.MyOrders;
+import com.tallty.smart_life_android.model.CartItem;
 import com.tallty.smart_life_android.model.Order;
 
 import org.greenrobot.eventbus.EventBus;
 
-import static android.R.attr.data;
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * 购物车-支付订单
  */
 public class PayOrder extends BaseBackFragment {
+    /**
+     * 微信支付渠道
+     */
+    private static final String CHANNEL_WECHAT = "wx";
+    /**
+     * 支付支付渠道
+     */
+    private static final String CHANNEL_ALIPAY = "alipay";
     // 结算数据
     private Order order = new Order();
+    private String payWay = "";
     // UI
     private TextView order_seq;
     private TextView order_postage;
     private TextView order_price_text;
     private TextView pay_btn;
     private TextView order_total_price;
+    private RadioGroup pay_radio_group;
 
     public static PayOrder newInstance(Order order) {
         Bundle args = new Bundle();
@@ -67,6 +92,7 @@ public class PayOrder extends BaseBackFragment {
         order_price_text = getViewById(R.id.order_price);
         pay_btn = getViewById(R.id.pay_now);
         order_total_price = getViewById(R.id.order_total_price);
+        pay_radio_group = getViewById(R.id.pay_radio_group);
     }
 
     @Override
@@ -95,7 +121,7 @@ public class PayOrder extends BaseBackFragment {
     }
 
     private void showData() {
-        order_price_text.setText("RMB " + (order.getPrice() - order.getPostage()));
+        order_price_text.setText("￥ " + (order.getPrice() - order.getPostage()));
         order_postage.setText("+ ￥ " + order.getPostage());
         order_seq.setText(order.getSeq());
         order_total_price.setText("￥ " + order.getPrice());
@@ -105,8 +131,46 @@ public class PayOrder extends BaseBackFragment {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.pay_now:
-
+                if (pay_radio_group.getCheckedRadioButtonId() == R.id.weixin_pay) {
+                    showToast("微信支付暂未开通");
+                    payWay = CHANNEL_WECHAT;
+                } else if (pay_radio_group.getCheckedRadioButtonId() == R.id.alipay){
+                    payWay = CHANNEL_ALIPAY;
+                    processPayOrder();
+                }
                 break;
         }
+    }
+
+    private void processPayOrder() {
+        showProgress("准备支付中...");
+        int amount = (int) (order.getPrice() * 100);
+        String body = "";
+        for (CartItem item : order.getCartItems()) {
+            String str = "【" + item.getName() + "x" + item.getCount() + "】";
+            body += str;
+        }
+        Map<String, String> fields = new HashMap<>();
+        fields.put("subject", order.getSeq());
+        fields.put("body", body);
+        Engine.authService(shared_token, shared_phone).getPayCharge(payWay, 1, fields)
+                .enqueue(new Callback<JsonElement>() {
+                    @Override
+                    public void onResponse(Call<JsonElement> call, Response<JsonElement> response) {
+                        hideProgress();
+                        if (response.isSuccessful()) {
+                            Log.d(App.TAG, String.valueOf(response.body().getAsJsonObject()));
+//                            Pingpp.createPayment(getActivity(), String.valueOf(response.body().getAsJsonObject()));
+                        } else {
+                            showToast("支付失败");
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<JsonElement> call, Throwable t) {
+                        hideProgress();
+                        showToast("网络错误");
+                    }
+                });
     }
 }
